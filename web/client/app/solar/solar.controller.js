5d839707a5874c55c07d5352124798c89b6dd4ea
centrevillage <centrevillage@gmail.com>
2016-08-22 11:24:21 +0900

    issue #21 画面全体ではなく、データのみリロードするよう修正

--- web/client/app/solar/solar.controller.js
+++ web/client/app/solar/solar.controller.js
@@ -1,22 +1,24 @@
 'use strict';
 
 angular.module('solarwebApp')
-  .controller('SolarCtrl', function ($scope, $http, $state, $timeout) {
-		$scope.solars = [];
-		$scope.summary = [];
+  .controller('SolarCtrl', function ($scope, $http, $interval) {
+    $scope.solars = [];
+    $scope.summary = [];
 
-		$http.get('/api/solars').success(function(solars){
-			$scope.solars = solars;
-		});
-		$http.get('/api/summary').success(function(summary){
-			$scope.summary = summary;
-		});
-		$http.get('/api/daily_solars/eco_megane/current').success(function(eco_megane_solars){
-			$scope.eco_megane_solars = eco_megane_solars;
-		});
-		$http.get('/api/daily_summaries/current').success(function(daily_summary){
-			$scope.daily_summary = daily_summary;
-		});
+    var reloadData = function() {
+      $http.get('/api/solars').success(function(solars){
+        $scope.solars = solars;
+      });
+      $http.get('/api/summary').success(function(summary){
+        $scope.summary = summary;
+      });
+      $http.get('/api/daily_solars/eco_megane/current').success(function(eco_megane_solars){
+        $scope.eco_megane_solars = eco_megane_solars;
+      });
+      $http.get('/api/daily_summaries/current').success(function(daily_summary){
+        $scope.daily_summary = daily_summary;
+      });
+    };
 
     $scope.autoReload = {
       value: false,
@@ -27,12 +29,16 @@ angular.module('solarwebApp')
       toggle: function() {
         this.value = !this.value;
         if (this.value) {
-          this.timer = $timeout(function() { $state.reload(); }, 5 * 60 * 1000);
+          this.timer = $interval(function() { 
+            reloadData();
+          }, 5 * 60 * 1000, 0);
         } else {
           if (this.timer !== null) {
-            $timeout.cancel(this.timer);
+            $interval.cancel(this.timer);
           }
         }
       }
     };
-	});
+
+    reloadData();
+  });
